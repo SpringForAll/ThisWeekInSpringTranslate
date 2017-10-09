b7b0aedf501a48aa1ab4f50e6936a0b2267d491c
strongant <877798942@qq.com>
2017-10-09 17:16:17 +0800

    添加生成二维码图片库

---
+++ officlialblog/src/test/java/QRImageGenerateTest.java
@@ -0,0 +1,36 @@
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+
+import java.io.*;
+
+/**
+ * 测试生成二维码图片
+ *
+ * @author <a href="mailto:baiwenhui@autobole.com">bwh</a>
+ * @see {http://kenglxn.github.io/QRGen/}
+ * @since 2017/10/9
+ */
+public class QRImageGenerateTest {
+
+    public static void main(String[] args) {
+
+        ByteArrayOutputStream bout =
+                QRCode.from("http://memorynotfound.com")
+                        .withSize(250, 250)
+                        .to(ImageType.PNG)
+                        .stream();
+
+        try {
+            OutputStream out = new FileOutputStream("/tmp/qr-code.png");
+            bout.writeTo(out);
+            out.flush();
+            out.close();
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
